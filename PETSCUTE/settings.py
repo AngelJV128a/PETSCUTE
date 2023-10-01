bc3350d5196e18d78e21adb15cf68f79e72e1be3
AngelJV128a <angelj.vazquez1@gmail.com>
2023-09-30 22:00:44 -0600

    Login 1.2

--- PETSCUTE/settings.py
+++ PETSCUTE/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PETSCUTE',
+    'MASCOTAS',
+    'PUBLICACIONES',
+    'USUARIOS',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'PETSCUTE.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'petscue-sql',
+        'USER': 'postgres',
+        'PASSWORD':'password#1',
+        'HOST':'project-database.postgres.database.azure.com',
+        'PORT':'5432',
     }
 }
 
--- PETSCUTE/views.py
+++ PETSCUTE/views.py
@@ -1,15 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate
+from . import models
 
 def login(request):
     return render (request,'login.html')
 
 def procesar(request):
     if request.method == 'POST':
-        nombre = request.POST.get('nombre')
         correo= request.POST.get('correo')
         # Haz algo con el valor 'nombre'
-        return HttpResponse(f"Nombre recibido: {nombre}, Correo: {correo}")
+        password = request.POST.get('password')
+        if models.Usuario.objects.filter(correo=correo,contrasenia=password).exists():
+            return render(request,'foro.html')
+        else:
+            contexto={
+                'Invalido': False
+            }
+            return render(request, 'login.html',contexto)
+    return render(request, 'procesar.html')
 
+def registrar(request):
+    if request.method == 'POST':
+        id=2
+        nombre=request.POST.get('nombre')
+        correo = request.POST.get('correo')
+        password= request.POST.get('password')
+        nuevoUsuario=models.Usuario()
+        nuevoUsuario.id=id
+        nuevoUsuario.nombre=nombre
+        nuevoUsuario.correo=correo
+        nuevoUsuario.password=password
+        nuevoUsuario.save()
     return render(request, 'procesar.html')
