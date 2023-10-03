9372de96fafd2b6e1f8119ad6423f3e9c988e5dc
KarlaSalazarS <131937269+KarlaSalazarS@users.noreply.github.com>
2023-10-03 01:48:27 +0000

    new file:   PETSCUTE/static/css/estilosm2.css new file:   PETSCUTE/static/img/equipo.png new file:   PETSCUTE/static/img/menu.png new file:   PETSCUTE/static/js/dark.js new file:   PETSCUTE/static/js/menu.js modified:   PETSCUTE/templates/login.html new file:   PETSCUTE/templates/menu2.html new file:   PETSCUTE/templates/perfil.html new file:   PETSCUTE/templates/publicar.html

---
+++ PETSCUTE/static/js/dark.js
@@ -0,0 +1,6 @@
+const swith = document.querySelector(".switch");
+
+swith.addEventListener("click", e=>{  /*Accedemos a la variable swith en la lista de clase cada que demos click se agregara o quitara la clase*/ 
+   swith.classList.toggle("active");
+   document.body.classList.toggle("active");
+})
