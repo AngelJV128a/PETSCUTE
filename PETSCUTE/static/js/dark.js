959c7f7ef34c347bb8cf6b67e8f438413ea280a4
AngelOrtega03 <126433826+AngelOrtega03@users.noreply.github.com>
2023-10-06 17:21:23 -0600

    Delete PETSCUTE directory

--- PETSCUTE/static/js/dark.js
+++
@@ -1,8 +0,0 @@
-//MODO OSCURO DEL INICIO
-const swith = document.querySelector(".switch");
-
-swith.addEventListener("click", e=>{  /*Accedemos a la variable swith en la lista de clase cada que demos click se agregara o quitara la clase*/ 
-   swith.classList.toggle("active");
-   document.body.classList.toggle("active");
-})
-
