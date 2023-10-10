f8ac112d3b0cd81f848aef843d32e4ddff30c7e4
AngelJV128a <angelj.vazquez1@gmail.com>
2023-10-09 22:19:54 -0600

    Perfil.html

--- PETSCUTE/views.py
+++ PETSCUTE/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 from . import models
 
 def login(request):
@@ -11,15 +11,18 @@ def procesar(request):
         correo= request.POST.get('correo')
         # Haz algo con el valor 'nombre'
         password = request.POST.get('password')
-        if models.Usuario.objects.filter(correo=correo,contrasenia=password).exists():
-            return render(request,'foro.html')
+        usuario=models.Usuario.objects.filter(correo=correo,contrasenia=password)
+        if usuario.exists():
+            request.session['username']=correo
+            request.session['password']=password
+            request.session['id']=usuario.first().id
+            return render(request,'menu2.html')
         else:
             contexto={
                 'Invalido': False
             }
             return render(request, 'login.html',contexto)
     return render(request, 'procesar.html')
-
 def registrar(request):
     if request.method == 'POST':
         nombre=request.POST.get('nombre')
@@ -30,4 +33,24 @@ def registrar(request):
         nuevoUsuario.correo=correo
         nuevoUsuario.contrasenia=password
         nuevoUsuario.save()
+    return render(request, 'login.html')
+
+def irAForo(request):
+    return render(request, 'foro.html')
+
+def irAPerfil(request):
+    return render(request, 'perfil.html')
+
+def irAInicio(request):
+    return render(request,'menu2.html')
+
+def irAPublicar(request):
+    return render(request,'publicar.html')
+def cerrarSesion(request):
+    username = request.session.get('username')
+    user_id = request.session.get('password')
+    print(username)
+    print(user_id)
+    print(request.session.get('id'))
+    request.session.flush()
     return render(request, 'login.html')
--- USUARIOS/views.py
+++ USUARIOS/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def updateUsuarios(request):
+
+    return render(request,'menu2.html')
